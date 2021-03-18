7cbe9a15ceb18fe6380bb938fb059b96f640297a
Simone Pellizzari <simone6021@gmail.com>
2021-03-19 00:21:06 +0100

    Compose file with some additional requirements in react app.

---
+++ app/src/setupProxy.js
@@ -0,0 +1,13 @@
+const createProxyMiddleware = require('http-proxy-middleware');
+
+const proxyHost = process.env.REACT_APP_PROXY_HOST || 'localhost';
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: `http://${proxyHost}:3080`,
+      changeOrigin: true
+    })
+  );
+};
